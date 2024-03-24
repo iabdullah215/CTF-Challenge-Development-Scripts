64009c3562b55cc09a8f7c1b824f89d916972b16
Muhammad Abdullah <muhammadabdullah8040@gmail.com>
2024-03-24 16:41:50 +0500

    Create file-generator.sh

---
+++ 4n 0ld Fri3nd/file-generator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+generate_random_string() {
+    characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    length=$(( RANDOM % 5 + 8 ))
+
+    string=''
+    for (( i=0; i<$length; i++ )); do
+        string+=${characters:RANDOM%${#characters}:1}
+    done
+
+    echo "$string"
+}
+
+for (( i=1; i<=20; i++ )); do
+    file_name="encrypted_data_$(date +%s%N | sha256sum | base64 | head -c 8).txt"
+
+    random_string=$(generate_random_string)
+
+    encrypted_string=$(echo "$random_string" | openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -salt -a -k AUCSS)
+
+    echo "$encrypted_string" > "$file_name"
+
+    echo "File '$file_name' created with encrypted data."
+done
