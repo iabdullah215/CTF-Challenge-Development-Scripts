20dbeca6acea4c38df98928b98233ce43cc7136b
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-06-12 21:56:56 +0500

    Update decrypt.sh

--- 4n 0ld Fri3nd/decrypt.sh
+++ 4n 0ld Fri3nd/decrypt.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "Will be provided after the CTF"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <file_name>"
+    exit 1
+fi
+
+file_name="$1"
+
+if [ ! -f "$file_name" ]; then
+    echo "Error: File '$file_name' not found."
+    exit 1
+fi
+
+# Read the encrypted data from the file
+encrypted_string=$(cat "$file_name")
+
+# Decryption flag variable
+decryption_key="TRUSTLINE"
+
+# Decrypt the data using OpenSSL
+decrypted_string=$(echo "$encrypted_string" | openssl enc -d -aes-256-cbc -pbkdf2 -iter 100000 -salt -a -k "$decryption_key")
+
+echo "Decrypted flag: $decrypted_string"
